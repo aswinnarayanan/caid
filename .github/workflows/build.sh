7d88d7d23be674da8e2c617635a9722783ee2ef3
Aswin Narayanan <aswin@excelsol.com>
2020-07-01 01:03:03 +1000

    Update build.sh

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -4,14 +4,14 @@ git config --local user.email "action@github.com"
 git config --local user.name "GitHub Action"
 git remote add github "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git"
 git pull github ${GITHUB_REF} --ff-only
-cd recipes/$1
+cd recipes/$APPLICATION
 /bin/bash build.sh
 git add .
 git commit -m "$GITHUB_SHA"
 git push github HEAD:${GITHUB_REF}
 for dockerfile in ./*.Dockerfile; do
   echo $dockerfile
-  IMAGEID=docker.pkg.github.com/${{ github.repository }}/$(basename $dockerfile .Dockerfile)
+  IMAGEID=docker.pkg.github.com/$GITHUB_REPOSITORY/$(basename $dockerfile .Dockerfile)
   IMAGEID=$(echo $IMAGEID | tr '[A-Z]' '[a-z]')
   echo "${{ secrets.GITHUB_TOKEN }}" | docker login docker.pkg.github.com -u ${{ github.actor }} --password-stdin
   docker pull $IMAGEID:latest || true
