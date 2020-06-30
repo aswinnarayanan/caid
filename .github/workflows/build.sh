d38600e15ad81277a5a520fde2dd324053a806fe
Aswin Narayanan <aswin@excelsol.com>
2020-07-01 01:37:00 +1000

    Rework github-ci workflows

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
+# Set git user to gh-actions service account
 git config --local user.email "action@github.com"
 git config --local user.name "GitHub Action"
+
+# Sync local repo with remote
 git remote add github "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git"
 git pull github ${GITHUB_REF} --ff-only
+
+# Build recipe
 cd recipes/$APPLICATION
 /bin/bash build.sh
+
+# Commmit and push recipe
 git add .
 git commit -m "$GITHUB_SHA"
 git push github HEAD:${GITHUB_REF}
+
+# Loop through Local Dockerfiles
+# Build and Push Dockerfile images
 for dockerfile in ./*.Dockerfile; do
-  echo $dockerfile
   IMAGEID=docker.pkg.github.com/$GITHUB_REPOSITORY/$(basename $dockerfile .Dockerfile)
   IMAGEID=$(echo $IMAGEID | tr '[A-Z]' '[a-z]')
   docker pull $IMAGEID:latest || true
